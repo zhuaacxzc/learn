01f0fb5b9bf9d3cf2d9f9ca4accc120d64246b5b
zhuaacxzc <1141589412@qq.com>
2022-07-03 16:00:11 +0800

    用printf函数输出一个由符号*组成的小飞机形状的图形

---
+++ practice1/practice1/practice1.c
@@ -0,0 +1,13 @@
+//用printf函数输出一个由符号*组成的小飞机形状的图形
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("     **     \n");
+	printf("     **     \n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *    \n");
+	printf("    *  *    \n");
+	return 0;
+}
