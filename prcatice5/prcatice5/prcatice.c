b8ff747171ac0b72e9036c3bf9cc5bd4aaf02ba2
zhuaacxzc <1141589412@qq.com>
2022-07-05 10:48:55 +0800

    辗转相除求最大公约数

---
+++ prcatice5/prcatice5/prcatice.c
@@ -0,0 +1,19 @@
+//辗转相除
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d", &a, &b);
+	while (c = a % b)
+	{
+		a = b;
+		b = c;
+	}
+	printf("最大公约数为：%d\n", b);
+	return 0;
+
+}
